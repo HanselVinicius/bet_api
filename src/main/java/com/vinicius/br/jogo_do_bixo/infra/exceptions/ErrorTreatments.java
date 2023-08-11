5aed2a1b9bd6c41567116758f2bae693b7e25509
Vinicius Hansel <hanelvinicius@gmail.com>
2023-08-11 17:59:11 -0300

    error treatments

---
+++ src/main/java/com/vinicius/br/jogo_do_bixo/infra/exceptions/ErrorTreatments.java
@@ -0,0 +1,55 @@
+package com.vinicius.br.jogo_do_bixo.infra.exceptions;
+
+import jakarta.persistence.EntityNotFoundException;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.http.converter.HttpMessageNotReadableException;
+import org.springframework.security.authentication.BadCredentialsException;
+import org.springframework.validation.FieldError;
+import org.springframework.web.bind.MethodArgumentNotValidException;
+import org.springframework.web.bind.annotation.ExceptionHandler;
+import org.springframework.web.bind.annotation.RestControllerAdvice;
+
+import javax.naming.AuthenticationException;
+
+@RestControllerAdvice
+public class ErrorTreatments {
+
+    @ExceptionHandler(EntityNotFoundException.class)
+    public ResponseEntity treatError404(){
+        return ResponseEntity.notFound().build();
+    }
+
+    @ExceptionHandler(MethodArgumentNotValidException.class)
+    public ResponseEntity treatError400(MethodArgumentNotValidException ex){
+        var errors = ex.getFieldErrors();
+        return ResponseEntity.badRequest().body(errors.stream().map(ValidationErrorDTO::new).toList());
+    }
+
+    @ExceptionHandler(HttpMessageNotReadableException.class)
+    public ResponseEntity tratarErro400(HttpMessageNotReadableException ex){
+        return ResponseEntity.badRequest().body(ex.getMessage());
+    }
+
+    @ExceptionHandler(BadCredentialsException.class)
+    public ResponseEntity treatBadCredentials(){
+        return ResponseEntity.status(HttpStatus.UNAUTHORIZED).build();
+    }
+    @ExceptionHandler(AuthenticationException.class)
+    public ResponseEntity treatDeniedAccess(){
+        return ResponseEntity.status(HttpStatus.FORBIDDEN).build();
+    }
+
+    @ExceptionHandler(Exception.class)
+    public ResponseEntity treatError500(Exception ex){
+        return ResponseEntity.status(HttpStatus.INTERNAL_SERVER_ERROR).build();
+    }
+
+
+    private record ValidationErrorDTO(String field, String message){
+        public ValidationErrorDTO(FieldError error){
+            this(error.getField(),error.getDefaultMessage());
+        }
+    }
+
+}
