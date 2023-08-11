027cf87be9cd4e33cabe37b977d63cb855bd3752
Vinicius Hansel <hanelvinicius@gmail.com>
2023-08-11 17:26:06 -0300

    random animal query

--- src/main/java/com/vinicius/br/jogo_do_bixo/models/animals/AnimalRepository.java
+++ src/main/java/com/vinicius/br/jogo_do_bixo/models/animals/AnimalRepository.java
@@ -2,7 +2,14 @@ package com.vinicius.br.jogo_do_bixo.models.animals;
 
 
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
 
 public interface AnimalRepository extends JpaRepository<Animal,Long> {
     Animal findByNome(String nome);
+
+    @Query("""
+            SELECT * FROM animals
+            ORDER BY RAND() LIMIT 1
+            """)
+    Animal findRandomAnimal();
 }
